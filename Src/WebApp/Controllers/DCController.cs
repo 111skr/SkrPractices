5cf38d2449b2f0abe3053d2a5066218b062e6b72
111skr <111skr@163.com>
2016-04-12 14:24:50 +0800

    add DC Controller

---
+++ Src/WebApp/Controllers/DCController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class DCController : Controller
+    {
+        // GET: DC
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string[] userids, string[] fields)
+        {
+
+            return View();
+        }
+    }
+}
