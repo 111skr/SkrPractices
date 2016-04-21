34d40fa88aa591ae5cc2a93a450db84b14502fa0
111skr <111skr@163.com>
2016-04-21 19:00:25 +0800

    实现顺序锁解决哲学家进餐问题；控制台通过输入控制执行

---
+++ Src/SkrConsole/PhilosophersEat/PhilosopherWithOrderChopstics.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkrConsole.PhilosophersEat
+{
+    public class PhilosopherWithOrderChopstics : Philosopher
+    {
+        public PhilosopherWithOrderChopstics(string name, Chopsticks left, Chopsticks right)
+            : base(name, left, right)
+        {
+            this.Name = name;
+            if (left.ID > right.ID)
+            {
+                this.left = right;
+                this.right = left;
+            }
+            else
+            {
+                this.left = left;
+                this.right = right;
+            }
+        }
+    }
+}
