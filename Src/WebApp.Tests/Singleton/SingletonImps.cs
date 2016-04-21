7412e67a879ff9fb06a49b8fb8f121844fbfa6e4
111skr <111skr@163.com>
2016-04-21 20:31:02 +0800

    增加单例并发测试

---
+++ Src/WebApp.Tests/Singleton/SingletonImps.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Tests.Singleton
+{
+    internal sealed class Singleton01
+    {
+        private static Singleton01 instance;
+
+        private Singleton01() { }
+
+        public static Singleton01 Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton01();
+                }
+                return instance;
+            }
+        }
+    }
+
+    internal sealed class Singleton02
+    {
+        // 在静态私有字段上声明单例
+        private static readonly Singleton02 instance = new Singleton02();
+
+        // 私有构造函数，确保用户在外部不能实例化新的实例
+        private Singleton02() { }
+
+        // 只读属性返回静态字段
+        public static Singleton02 Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        //标记类为sealed是好的，可以防止被集成，然后在子类实例化，
+        //使用在静态私有字段上通过new的形式，来保证在该类第一次被调用的时候创建实例，是不错的方式，
+        //但有一点需要注意的是，C#其实并不保证实例创建的时机，因为C#规范只是在IL里标记该静态字段是BeforeFieldInit，
+        //也就是说静态字段可能在第一次被使用的时候创建，也可能你没使用了，它也帮你创建了，也就是周期更早，我们不能确定到底是什么创建的实例。
+    }
+
+    public sealed class Singleton03
+    {
+        // 依然是静态自动hold实例
+        private static volatile Singleton03 instance = null;
+        // Lock对象，线程安全所用
+        private static object syncRoot = new Object();
+
+        private Singleton03() { }
+
+        public static Singleton03 Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new Singleton03();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        //使用volatile来修饰，是个不错的注意，确保instance在被访问之前被赋值实例，一般情况都是用这种方式来实现单例。
+        //volatile修饰符的作用需要研究一下
+    }
+
+    public class Singleton04
+    {
+        // 因为下面声明了静态构造函数，所以在第一次访问该类之前，new Singleton()语句不会执行
+        private static readonly Singleton04 _instance = new Singleton04();
+
+        public static Singleton04 Instance
+        {
+            get { return _instance; }
+        }
+
+        private Singleton04()
+        {
+        }
+
+        // 声明静态构造函数就是为了删除IL里的BeforeFieldInit标记
+        // 以去北欧静态自动在使用之前被初始化
+        static Singleton04()
+        {
+        }
+    }
+
+    public sealed class Singleton05
+    {
+        private Singleton05()
+        {
+        }
+
+        public static Singleton05 Instance { get { return Nested._instance; } }
+
+        private class Nested
+        {
+            static Nested()
+            {
+            }
+
+            internal static readonly Singleton05 _instance = new Singleton05();
+        }
+    }
+
+    public class Singleton06
+    {
+        // 因为构造函数是私有的，所以需要使用lambda
+        private static readonly Lazy<Singleton06> _instance = new Lazy<Singleton06>(() => new Singleton06());
+
+        private Singleton06()
+        {
+        }
+
+        public static Singleton06 Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+    }
+}
---
+++ Src/WebApp.Tests/Singleton/SingletonTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Shouldly;
+using Xunit.Abstractions;
+using System.Threading;
+
+namespace WebApp.Tests.Singleton
+{
+    public class SingletonTest
+    {
+        #region 初始化
+        private readonly ITestOutputHelper output;
+        public SingletonTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+        #endregion
+
+        void DoConcurrentCreateTest<T>(int concurrentCount, Func<T> creator)
+        {
+            List<Task<T>> taskList = new List<Task<T>>();
+            List<T> instances = new List<T>();
+
+            Parallel.For(0, concurrentCount, i =>
+            {
+                var obj = creator();
+                instances.Add(obj);
+                output.WriteLine("iterator index :[{0}], thread :[{1}], obj hash code: {2}.", i, Thread.CurrentThread.ManagedThreadId, obj.GetHashCode());
+            });
+
+            //for (int i = 0; i < concurrentCount; i++)
+            //{
+            //    taskList.Add(Task.Factory.StartNew<T>(creator));
+            //}
+
+            //taskList.ForEach(t =>
+            //{
+            //    if (!t.IsFaulted)
+            //    {
+            //        instances.Add(t.Result);
+            //    }
+            //});
+
+            instances.ForEach(item =>
+            {
+                item.ShouldBe<T>(creator());
+            });
+        }
+
+        [Fact(DisplayName = "v1-没有保护，多线程并发创建会产生多示例，不适应用在创建消耗高单例对象")]
+        public void Singleton01_should_not_get_only_one_instance_with_concurrent_create()
+        {
+            DoConcurrentCreateTest(30, () => Singleton01.Instance);
+        }
+
+        [Fact(DisplayName = "v2-并发创建无问题")]
+        public void Singleton02_should_get_only_one_instance_with_concurrent_create()
+        {
+            DoConcurrentCreateTest(30, () => Singleton02.Instance);
+        }
+
+        [Fact(DisplayName = "v3-并发创建无问题")]
+        public void Singleton03_should_get_only_one_instance_with_concurrent_create()
+        {
+            DoConcurrentCreateTest(30, () => Singleton03.Instance);
+        }
+
+        [Fact(DisplayName = "v4-并发创建无问题")]
+        public void Singleton04_should_get_only_one_instance_with_concurrent_create()
+        {
+            DoConcurrentCreateTest(30, () => Singleton04.Instance);
+        }
+
+        [Fact(DisplayName = "v5-并发创建无问题")]
+        public void Singleton05_should_get_only_one_instance_with_concurrent_create()
+        {
+            DoConcurrentCreateTest(30, () => Singleton05.Instance);
+        }
+
+        [Fact(DisplayName = "v6-并发创建无问题")]
+        public void Singleton06_should_get_only_one_instance_with_concurrent_create()
+        {
+            DoConcurrentCreateTest(30, () => Singleton06.Instance);
+        }
+    }
+}
