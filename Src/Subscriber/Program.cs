492b241c8ee3f832e85f8808b5c13c40ed84907b
111skr <111skr@163.com>
2016-07-26 15:16:19 +0800

    实现RabbitMq的单Queue发布；单Queue订阅，windows服务

---
+++ Src/Subscriber/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "192.168.87.187",
+                Port = 35672,
+                UserName = "ibitauto",
+                Password = "ibitauto",
+                VirtualHost = "VH-Test"
+            };
+            using (var connection = factory.CreateConnection())
+            {
+                var queueName = "q-hello";
+
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: queueName,
+                                         durable: true,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                    };
+                    channel.BasicConsume(queue: queueName,
+                                         noAck: true,
+                                         consumer: consumer);
+
+
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Src/WindowsService1/RabbitMqSubscriberService.cs
@@ -0,0 +1,72 @@
+﻿using System.ServiceProcess;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using NLog;
+
+namespace WindowsService1
+{
+    public partial class RabbitMqSubscriberService : ServiceBase
+    {
+        private IModel channel;
+        private readonly ILogger logger;
+        private readonly string queueName;
+
+        public RabbitMqSubscriberService(IModel channel, NLog.ILogger logger, string queueName = "q-hello")
+        {
+            InitializeComponent();
+
+            this.channel = channel;
+            this.logger = logger;
+            this.queueName = queueName;
+
+            logger.Info("{0} init", this.GetType().ToString());
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            logger.Info("start");
+
+            string queueName = "q-hello",
+                   exchangeName = "ex_hello";
+
+            channel.ExchangeDeclare(exchangeName, "fanout", true, false, null);
+
+            channel.QueueDeclare(queue: queueName,
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            channel.QueueBind(queueName, exchangeName, string.Empty);
+
+            channel.BasicQos(0, 1, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (c, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                logger.Info("Received from queue:{1} msg:{0}", message, queueName);
+                (c as EventingBasicConsumer).Model.BasicAck(ea.DeliveryTag, false);
+            };
+
+            consumer.Registered += (model, ea) =>
+            {
+                logger.Info("Registered :{0}", ea.ConsumerTag);
+            };
+
+            channel.BasicConsume(queue: queueName,
+                                 noAck: false,
+                                 consumer: consumer);
+        }
+
+
+
+        protected override void OnStop()
+        {
+            logger.Info("stop");
+            channel.Close();
+        }
+    }
+}
